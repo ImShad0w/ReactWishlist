6eb4b2b1db05315f4c3828cf8cbc17f35c51fa49
shxd0w <kirillpashnin.dev@gmail.com>
2025-11-23 18:59:44 +0100

    Added zustand store for wishlist

--- src/stores/animeStore.tsx
+++ src/stores/animeStore.tsx
@@ -1,1 +1,32 @@
 import { create } from "zustand";
+import type { Anime } from "@tutkli/jikan-ts";
+
+interface AnimeStore {
+  wishlist: Anime[];
+  currentAnime: Anime | null;
+  addToWishlist: (anime: Anime) => void;
+  removeFromWishlist: (id: number) => void;
+  isInWishlist: (id: number) => boolean;
+  setCurrentAnime: (anime: Anime) => void;
+}
+
+export const useAnimeStore = create<AnimeStore>((set, get) => ({
+  wishlist: [],
+  currentAnime: null,
+
+  addToWishlist: (anime) =>
+    set((state) => ({
+      wishlist: [...state.wishlist, anime],//From previous state, adds a new anime to it
+    })),
+
+  removeFromWishlist: (id) =>
+    set((state) => ({
+      wishlist: state.wishlist.filter((a) => a.mal_id !== id),//From previous state remove anime by id for current state
+    })),
+
+  isInWishlist: (id) => {
+    return get().wishlist.some((a) => a.mal_id === id);
+  },
+
+  setCurrentAnime: (anime) => set({ currentAnime: anime }),
+}));
